ae6cb35fc0500665ec492c3565107f8cdd9e9f1a
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-10 13:36:47 +0400

    Styled-components for components and their styles is added

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -5,7 +5,7 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
           ${props => props.height},
-          calc(25vw/ ${props => props.width}));
+          calc(25vw / ${props => props.width}));
   grid-template-columns: repeat(
           ${props => props.width}, 1fr);
   grid-gap: 1px;

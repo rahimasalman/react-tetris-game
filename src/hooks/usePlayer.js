e529f0becbe0aef108ff4e70a362eb88d9c2f036
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-11 16:23:53 +0400

    resetPlayer and updatePlayerpos function is added

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import {useState} from "react";
+import {useState, useCallback} from "react";
 
 import {randomTetromino} from "../tetrominos";
+import {STAGE_WIDTH} from "../gameHelpers";
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -8,5 +9,24 @@ export const usePlayer = () => {
         tetromino: randomTetromino().shape,
         collided: false,
     });
-    return [player];
+
+    const updatePlayerPos = ({x, y, collided}) => {
+        setPlayer(prev => ({
+            ...prev,
+            pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)},
+            collided,
+        }));
+    };
+
+    const resetPlayer = useCallback(() => {
+           setPlayer({
+               pos: {x: STAGE_WIDTH / 2 -2, y: 0},
+               tetromino: randomTetromino().shape,
+               collided: false,
+           });
+        }, [],
+    );
+
+
+    return [player, updatePlayerPos, resetPlayer];
 };

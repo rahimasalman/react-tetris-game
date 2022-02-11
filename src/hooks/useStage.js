9fcb41c23402f6b0ba85dab89813cc9c51dfe1d4
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-11 16:53:09 +0400

    added movement functionality to tetrominos

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,32 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {createStage} from "../gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
         const [stage, setStage] = useState(createStage());
+
+        useEffect(() =>{
+            const updateStage = prevStage => {
+              //Flushing the stage is the first step
+                const newStage = prevStage.map(row =>
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),);
+
+                //Draw a tetromino after
+                player.tetromino.forEach((row, y) => {
+                    row.forEach((value, x) => {
+                        if(value !== 0 ) {
+                            newStage[y + player.pos.y][x + player.pos.x] = [
+                                value,
+                                `${player.collided ? 'merged' : 'clear'}`,
+                            ];
+                        }
+                    });
+                });
+
+                return newStage;
+            };
+            setStage(prev => updateStage(prev) );
+
+    }, [ player]);
+
         return [stage, setStage];
     };

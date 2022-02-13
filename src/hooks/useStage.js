a127798914d2926119e1cdd43e1ab52dcb30a432
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-13 20:23:53 +0400

    add playerRotate function

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,36 +1,35 @@
-import {useState, useEffect} from "react";
-import {createStage} from "../gameHelpers";
+import { useState, useEffect } from "react";
+import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
-        const [stage, setStage] = useState(createStage());
+  const [stage, setStage] = useState(createStage());
 
-        useEffect(() =>{
-            const updateStage = prevStage => {
-              //Flushing the stage is the first step
-                const newStage = prevStage.map(row =>
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),);
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      //Flushing the stage is the first step
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
 
-                //Draw a tetromino after
-                player.tetromino.forEach((row, y) => {
-                    row.forEach((value, x) => {
-                        if(value !== 0 ) {
-                            newStage[y + player.pos.y][x + player.pos.x] = [
-                                value,
-                                `${player.collided ? 'merged' : 'clear'}`,
-                            ];
-                        }
-                    });
-                });
-            // check for collision
-                if(player.collided) {
-                    resetPlayer();
-                };
-
-                return newStage;
-            };
-            setStage(prev => updateStage(prev) );
-
-    }, [ player.collided, player.pos.x, player.pos.y, player.tetromino]);
-
-        return [stage, setStage];
+      //Draw a tetromino after
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      // check for collision
+      if (player.collided) {
+        resetPlayer();
+      }
+      return newStage;
     };
+    setStage((prev) => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage];
+};
